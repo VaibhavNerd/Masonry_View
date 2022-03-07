094aeffa08a04e57cfb7754d617e5f693aa74030
vaibh <vaibhavdudeperfect@gmail.com>
2022-03-07 21:10:07 +0530

    Added Image Data

---
+++ lib/image_data.dart
@@ -0,0 +1,120 @@
+import 'package:meta/meta.dart';
+
+class ImageData {
+  final String id;
+  final String imageUrl;
+
+  const ImageData({
+    required this.id,
+    required this.imageUrl,
+  });
+}
+
+const imageList = [
+  ImageData(
+    id: 'id-001',
+    imageUrl: 'https://images.unsplash.com/photo-1640622842223-e1e39f4bf627?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-002',
+    imageUrl: 'https://images.unsplash.com/photo-1640622299485-7fef00b3dc24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-003',
+    imageUrl: 'https://images.unsplash.com/photo-1646661125446-2fe845fd715c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-004',
+    imageUrl: 'https://images.unsplash.com/photo-1646667342839-dd71eb16a413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-005',
+    imageUrl: 'https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-006',
+    imageUrl: 'https://i.ibb.co/hRKGyB2/Frame-11.png',
+  ),
+  ImageData(
+    id: 'id-007',
+    imageUrl: 'https://images.unsplash.com/photo-1646718162561-825e69902191?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-008',
+    imageUrl: 'https://picsum.photos/seed/image008/500/700',
+  ),
+  ImageData(
+    id: 'id-009',
+    imageUrl: 'https://images.unsplash.com/photo-1644982647708-0b2cc3d910b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-010',
+    imageUrl: 'https://picsum.photos/seed/image010/500/900',
+  ),
+  ImageData(
+    id: 'id-011',
+    imageUrl: 'https://images.unsplash.com/photo-1646725874577-884b67c2a5d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-012',
+    imageUrl: 'https://picsum.photos/seed/image012/500/700',
+  ),
+  ImageData(
+    id: 'id-013',
+    imageUrl: 'https://i.ibb.co/fYwg8HV/240411263-1752745358246805-3465666750072308349-n.jpg',
+  ),
+  ImageData(
+    id: 'id-014',
+    imageUrl: 'https://images.unsplash.com/photo-1640622659144-18a1fd885123?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-015',
+    imageUrl: 'https://picsum.photos/seed/image015/500/500',
+  ),
+  ImageData(
+    id: 'id-016',
+    imageUrl: 'https://images.unsplash.com/photo-1646620677329-5c5f6bee7257?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60+',
+  ),
+  ImageData(
+    id: 'id-017',
+    imageUrl: 'https://picsum.photos/seed/image017/500/600',
+  ),
+  ImageData(
+    id: 'id-018',
+    imageUrl: 'https://picsum.photos/seed/image018/500/900',
+  ),
+  ImageData(
+    id: 'id-019',
+    imageUrl: 'https://picsum.photos/seed/image019/500/800',
+  ),
+  ImageData(
+    id: 'id-020',
+    imageUrl: 'https://media.istockphoto.com/photos/female-army-solider-saluting-against-sunset-sky-picture-id1300072335?b=1&k=20&m=1300072335&s=170667a&w=0&h=HlilgHaJ1QxX507ncfFyqSyqDZwVf_hvwSbeeiyNJAQ=',
+  ),
+  ImageData(
+    id: 'id-023',
+    imageUrl: 'https://media.istockphoto.com/photos/abstract-graphic-world-map-illustration-on-blue-background-big-data-picture-id1294021851?b=1&k=20&m=1294021851&s=170667a&w=0&h=vsypj3JPqiWOU5q21fX3lHt1Z7wphVNE8kfqdpogPSs=',
+  ),
+  ImageData(
+    id: 'id-021',
+    imageUrl: 'https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dXJsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    // imageUrl: 'https://media.istockphoto.com/photos/female-army-solider-saluting-against-sunset-sky-picture-id1300072335?b=1&k=20&m=1300072335&s=170667a&w=0&h=HlilgHaJ1QxX507ncfFyqSyqDZwVf_hvwSbeeiyNJAQ=',
+  ),
+  ImageData(
+    id: 'id-022',
+    imageUrl: 'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXJsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+    id: 'id-024',
+    imageUrl: 'https://images.unsplash.com/photo-1574285013029-29296a71930e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmVydGljYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+  ImageData(
+      id: 'id-025',
+      imageUrl:'https://images.unsplash.com/photo-1505521377774-103a8cc2f735?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dmVydGljYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+  ),
+  ImageData(
+      id: 'id-026',
+      imageUrl: 'https://images.unsplash.com/photo-1564698010692-0fe284aae806?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dmVydGljYWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+
+];
